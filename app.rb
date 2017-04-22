94227ae1a95b6da6eafe610b4dcbefda7025018f
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-04-22 09:56:52 -0400

    Enciphers message

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require_relative "lib/caesar"
 
 get '/' do
-	erb :index
+	message = params["Message"]
+	shift = params["Shift"]
+	unless message.nil? | shift.nil?
+		shift = shift.to_i
+		encrypted_message = caesar(message, shift)
+	end
+	erb :index, :locals => {:encrypted_message => encrypted_message}
 end
