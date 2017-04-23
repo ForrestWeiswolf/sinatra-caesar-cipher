501e48a7d26bae37386f2799dafac3c2603c4ac2
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-04-23 09:02:22 -0400

    New route for encrypted message

--- app.rb
+++ app.rb
@@ -2,11 +2,15 @@ require 'sinatra'
 require_relative "lib/caesar"
 
 get '/' do
+	erb :index
+end
+
+get '/encrypt' do
 	message = params["Message"]
 	shift = params["Shift"]
 	unless message.nil? | shift.nil?
 		shift = shift.to_i
 		encrypted_message = caesar(message, shift)
 	end
-	erb :index, :locals => {:encrypted_message => encrypted_message}
+	erb :encrypted, :locals => {:encrypted_message => encrypted_message}
 end
